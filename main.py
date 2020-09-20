4ce4497f54876e2b117fbe9e240bc530e47c6d75
Mais1212 <dragon_bord@mail.ru>
2020-09-20 20:53:08 +0300

    some fix

--- main.py
+++ main.py
@@ -3,33 +3,41 @@ from urllib.parse import urljoin
 import requests
 import lxml
 
-#Сайт с которого берем книги
-HOST = "https://yummyanime.club/catalog"
+#Сайт с которого берем Анимешки
+HOST = "https://yummyanime.club"
 #User-Agent, без него на сайт не пустят
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 page_number = 0
 
 def get_content(pages_content) :
 	anime_links = []
-	selector_url = ".image-block"
 	for anime in pages_content:
-		anime_links.append(urljoin(HOST, str(pages_content.select_one(selector_url))))
+		anime_links.append(anime.get('href'))
 	return anime_links
 
 
-
-
-
 #Бежим по всем страницам
-while page_number < 2:
-	page_number += 1 
+while page_number < 2: 
 
-	url_page = HOST + "?page="  + str(page_number)
-	response_page = requests.get(url_page, headers=HEADERS)
-	soup_page = BeautifulSoup(response_page.text, "lxml")
+	url_page = HOST + "/catalog?page="  + str(page_number)
+	response_main_page = requests.get(url_page, headers=HEADERS)
+	soup_min_page = BeautifulSoup(response_main_page.text, "lxml")
 
-	selector_content = ".anime-column"
-	pages_content = soup_page.select(selector_content)
+	selector_content = ".anime-column .image-block"
+	pages_content = soup_min_page.select(selector_content)
 	anime_links = get_content(pages_content)
 
-	print(anime_links)
+	for link in anime_links :
+		response_anime_page = requests.get(HOST + link, headers=HEADERS)
+		soup_anime_page = BeautifulSoup(response_main_page.text, "lxml")
+
+		selector_name = "div.content div div h1"
+		select_name = soup_anime_page.select(selector_name)
+		print(select_name)
+		
+
+
+
+
+
+	page_number += 1
