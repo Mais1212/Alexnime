46a33d4a90597299a318025666a41423b45ea643
Mais1212 <dragon_bord@mail.ru>
2020-09-26 12:30:03 +0300

    Result in xlsx

--- main.py
+++ main.py
@@ -1,47 +1,138 @@
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-from selenium import webdriver
+import argparse
+import sys
+import xlwt
+import csv
 import requests
 import html.parser
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
  
 # Сайт с которого берем Анимешки
 HOST = "https://yummyanime.club"
 # User-Agent, без него на сайт не пустят
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-page_number = 0
- 
- 
+PROXIES={"http":"http://10.10.1.10:3128"}
+anime_data = []
+
+def createParser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-sp",
+        "--start_page",
+        type= int,
+        help = "Какой страницой начать?",
+        default = 1
+     )
+
+    parser.add_argument(
+        "-ep",
+        "--end_page",
+        type = int,
+        help = "Какой страницой закончить?",
+        default = 2
+        )
+    return parser
+
+parser = createParser()
+namespace = parser.parse_args()
+
+
+start_page = namespace.start_page
+end_page = namespace.end_page
+
+def create_excel(path="Результат.xlsx", anime_data=[""]):
+    count = 1
+    book = xlwt.Workbook('utf8')
+    # Создаем шрифты
+    main_font = xlwt.easyxf('font: height 240,name Fixedsys,colour_index black, bold off,\
+        italic off; align: wrap on, vert top, horiz left;\
+        borders: top_color black, bottom_color black, right_color black, left_color black,\
+        left thin, right thin, top thin, bottom thin;\
+        pattern: pattern solid, fore_colour green;')
+
+    anime_font = xlwt.easyxf('font: height 240,name Comic Sans MS ,colour_index black, bold off,\
+        italic off; align: wrap on, vert top, horiz left;\
+        borders: top_color black, bottom_color black, right_color black, left_color black,\
+        left thin, right thin, top thin, bottom thin;\
+        pattern: pattern solid, fore_colour yellow; alignment: ')
+    # Добавляем лист
+    sheet = book.add_sheet('Anine')
+
+    # Заполняем ящейку (Строка, Колонка, Текст, Шрифт)
+    sheet.write(0,0,'Название',main_font)
+    sheet.write(0,1,'Год выхода',main_font)
+    sheet.write(0,2,'Жанры',main_font)
+    sheet.write(0,3,'Возрастной рейтинг',main_font)
+    sheet.write(0,4,'Оценка пользователей',main_font)
+    sheet.write(0,5,'Озвучка',main_font)
+
+    for anime in anime_data:
+        sheet.write(count,0,anime[0],anime_font)
+        sheet.write(count,1,anime[1],anime_font)
+        sheet.write(count,2,anime[2],anime_font)
+        sheet.write(count,3,anime[3],anime_font)
+        sheet.write(count,4,anime[4]+'⭐',anime_font)
+        sheet.write(count,5,anime[5],anime_font)
+        count += 1
+
+    # Высота строки
+    sheet.row(1).height = 3000
+    # Ширина колонки
+    sheet.col(0).width = 3000
+    # Лист в положении "альбом"
+    sheet.portrait = False
+    # Масштабирование при печати
+    sheet.set_print_scaling(85)
+    # Сохраняем в файл
+    book.save(path)
+
 def get_content(pages_content):
     anime_links = []
     for anime in pages_content:
         anime_links.append(anime.get('href'))
     return anime_links
- 
- 
+
 # Бежим по всем страницам
-while page_number < 2:
- 
-    url_page = HOST + "/catalog?page=" + str(page_number)
-    response_main_page = requests.get(url_page, headers=HEADERS)
+while start_page < end_page:
+
+    url_page = HOST + "/catalog?page=" + str(start_page)
+    response_main_page = requests.get(url_page, headers=HEADERS, timeout=(0.1, 10), proxies=PROXIES)
     soup_min_page = BeautifulSoup(response_main_page.content, "html.parser")
-	 
+
     selector_content = ".anime-column .image-block"
     pages_content = soup_min_page.select(selector_content)
     anime_links = get_content(pages_content)
     count = 0
     for link in anime_links:
 
-        response_anime_page = requests.get(HOST + link, headers=HEADERS)
+        response_anime_page = requests.get(HOST + link, headers=HEADERS, proxies=PROXIES)
         soup_anime_page = BeautifulSoup(response_anime_page.content, "html.parser")
- 
-        selector_name = "div.anime-column-info a"
-        selector_age = "span.year-block"
-        selector_age_rating = "ul.content-main-info li" #<li><span>Возрастной рейтинг:</span> PG-13 (от 13 лет)</li>
-        select_name = soup_anime_page.select(selector_name)
-        select_age = soup_anime_page.select(selector_age)
-        selector_age_rating = soup_anime_page.select(selector_age_rating)
-        print(selector_age_rating.text)
+
+        print(link)
+        #Что нужно взять со страницы
+        selector_name = ".content div div h1"
+        selector_age = ".content-main-info li:nth-child(3)"
+        selector_age_rating = ".content-main-info li:nth-child(5)"
+        selector_ganres = ".content-main-info .categories-list"
+        selector_rating = "span.main-rating-block > span.main-rating"
+        selecror_voices = "ul.animeVoices > li"
+        #Процесс "Взятия" со страницы
+        select_name = soup_anime_page.select_one(selector_name).text
+        select_age = soup_anime_page.select_one(selector_age).text
+        select_age_rating = soup_anime_page.select_one(selector_age_rating).text
+        select_ganres = soup_anime_page.select_one(selector_ganres).text
+        select_rating = soup_anime_page.select_one(selector_rating).text
+        try:
+            select_voices = soup_anime_page.select_one(selecror_voices).text
+        except AttributeError:
+            select_voices = "Не хватает данных"
+        
+        full_select = [select_name, select_age, select_ganres, select_age_rating, select_rating, select_voices]
+        anime_data.append(full_select)
         count += 1
- 
-    page_number += 1
+
+    start_page += 1
+
+create_excel("Результат.xlsx",anime_data)
